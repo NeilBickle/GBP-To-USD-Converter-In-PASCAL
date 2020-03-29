7ea6ef659cfa3892a38d868071b85ed667064516
Neil Bickle (AMBCS) (AssocIScT) (TMIET) (ICTTech) <43986316+NeilBickle@users.noreply.github.com>
2020-03-29 13:44:08 +0100

    Created the PASCAL File
    1. Created The Pascal Files

---
+++ main.pas
@@ -0,0 +1,10 @@
+program GBPTOUSD;
+
+var 
+	a:integer;
+begin
+  writeln('Welcome to the GBP To USD convertor using the PASCAL programming language');
+	writeln('Please Enter A Amount Of GBP: ');
+	readln(a);
+	writeln(a * 1.25);
+end.
